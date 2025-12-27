5273caff450dec38a2b8f7dd0ebd5b473def7d42
floyare <neptuneeex@gmail.com>
2025-12-27 16:43:36 +0100

    fix: fixed file saving dialog

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,17 +2,49 @@ use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder};
 use tauri_plugin_notification::NotificationExt;
 use tauri::UserAttentionType;
 use tauri_plugin_opener::OpenerExt;
+use tauri_plugin_dialog::DialogExt;
+use std::fs;
+use std::io::Write;
 
 const LINK_REDIRECT_SCRIPT: &str = r#"
 (function (){
     console.log('Link observer started')
 
-    document.addEventListener('click', (e) => {
+    document.addEventListener('click', async (e) => {
         const anchor = e.target.closest('a');
         if(!anchor) return;
 
         if (anchor.getAttribute("role") == "link" && anchor.target === "_blank") {
             e.preventDefault();
+
+            if(anchor.href.startsWith("blob:")) {
+                const url = anchor.href;
+                try {
+                    const filename = anchor.getAttribute("download") || "pobrany_plik";
+                    const response = await fetch(url);
+                    const blob = await response.blob();
+                    const arrayBuffer = await blob.arrayBuffer();
+                    const uint8Array = new Uint8Array(arrayBuffer);
+                    const data = Array.from(uint8Array);
+
+                    if (!filename.includes('.')) {
+                        const type = blob.type.split('/')[1];
+                        if (type) filename += `.${type}`;
+                    }
+
+                    if (window.__TAURI__) {
+                        window.__TAURI__.core.invoke('save_blob', { 
+                            fileName: filename, 
+                            fileData: data 
+                        });
+                    }
+                } catch (e) {
+                    console.error("Failed while saving blob:", e);
+                    alert("Failed while downloading this file.");
+                }
+
+                return
+            }
 
             if (window.__TAURI__ && window.__TAURI__.core) {
                 window.__TAURI__.core.invoke('open_link', { link: anchor.getAttribute("href") })
@@ -83,12 +115,37 @@ fn open_link(app: AppHandle, link: String) {
     app.opener().open_path(link, None::<&str>);
 }
 
+#[tauri::command]
+async fn save_blob(app: AppHandle, file_name: String, file_data: Vec<u8>) -> Result<(), String> {
+    let file_path = app.dialog()
+        .file()
+        .set_file_name(&file_name)
+        .set_title("Save file")
+        .add_filter("All Files", &["*"])
+        .blocking_save_file();
+
+    if let Some(path) = file_path {
+        match fs::File::create(path.into_path().unwrap()) {
+            Ok(mut file) => {
+                if let Err(e) = file.write_all(&file_data) {
+                    return Err(format!("Failed while saving: {}", e));
+                }
+                Ok(())
+            }
+            Err(e) => Err(format!("Failed while creating file: {}", e)),
+        }
+    } else {
+        Ok(())
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![notify_command, open_link])
+        .plugin(tauri_plugin_dialog::init())
+        .invoke_handler(tauri::generate_handler![notify_command, open_link, save_blob])
         .setup(|app| {
             let win = WebviewWindowBuilder::new(
                 app,
